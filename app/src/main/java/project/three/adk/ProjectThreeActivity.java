19e7e18466a616d219feb065a3a4fd6920fd2402
jordane-quincy <jordanequincy@gmail.com>
2016-04-29 19:08:42 +0200

    cleanup code de base

--- app/src/main/java/project/three/adk/ProjectThreeActivity.java
+++ app/src/main/java/project/three/adk/ProjectThreeActivity.java
@@ -1,61 +1,118 @@
 package project.three.adk;
 
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.os.Vibrator;
 import android.util.Log;
 import android.widget.TextView;
 
-//import com.android.future.usb.UsbAccessory;
-//import com.android.future.usb.UsbManager;
-import android.hardware.usb.UsbAccessory;
-import android.hardware.usb.UsbManager;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ProjectThreeActivity extends Activity {
 
 	private static final String TAG = ProjectThreeActivity.class.getSimpleName();
-
-	private PendingIntent mPermissionIntent;
 	private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
-	private boolean mPermissionRequestPending;
-
-	private UsbManager mUsbManager;
-	private UsbAccessory mAccessory;
-	private ParcelFileDescriptor mFileDescriptor;
-	private FileInputStream mInputStream;
-	private FileOutputStream mOutputStream;
-
 	private static final byte COMMAND_BUTTON = 0x1;
 	private static final byte TARGET_BUTTON = 0x1;
 	private static final byte VALUE_ON = 0x1;
 	private static final byte VALUE_OFF = 0x0;
-	
 	private static final String BUTTON_PRESSED_TEXT = "The Button is pressed!";
 	private static final String BUTTON_NOT_PRESSED_TEXT = "The Button is not pressed!";
+    private PendingIntent mPermissionIntent;
+    private boolean mPermissionRequestPending;
+    private UsbManager mUsbManager;
+    private UsbAccessory mAccessory;
+    private ParcelFileDescriptor mFileDescriptor;
+    private FileInputStream mInputStream;
+    private FileOutputStream mOutputStream;
+    private TextView buttonStateTextView;
 
-	private TextView buttonStateTextView;
-	
 	private Vibrator vibrator;
 	private boolean isVibrating;
+    Runnable commRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            int ret = 0;
+            final byte[] buffer = new byte[3];
+
+            while (ret >= 0) {
+                try {
+                    ret = mInputStream.read(buffer);
+                } catch (IOException e) {
+                    break;
+                }
+
+                switch (buffer[0]) {
+                    case COMMAND_BUTTON:
+
+                        if (buffer[1] == TARGET_BUTTON) {
+                            if (buffer[2] == VALUE_ON) {
+                                startVibrate();
+                            } else if (buffer[2] == VALUE_OFF) {
+                                stopVibrate();
+                            }
+                            runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    buttonStateTextView.setText(buffer[2] == VALUE_ON ? BUTTON_PRESSED_TEXT : BUTTON_NOT_PRESSED_TEXT);
+                                }
+                            });
+                        }
+                        break;
+
+                    default:
+                        Log.d(TAG, "unknown msg: " + buffer[0]);
+
+                        break;
+                }
+            }
+        }
+    };
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbAccessory accessory = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+                    if (intent.getBooleanExtra(
+                            UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        openAccessory(accessory);
+                    } else {
+                        Log.d(TAG, "permission denied for accessory "
+                                + accessory);
+                    }
+                    mPermissionRequestPending = false;
+                }
+            } else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
+                UsbAccessory accessory = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+                if (accessory != null && accessory.equals(mAccessory)) {
+                    closeAccessory();
+                }
+            }
+        }
+    };
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE); //UsbManager.getInstance(this);
-		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
+        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
 				ACTION_USB_PERMISSION), 0);
 		IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
 		filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
@@ -63,8 +120,8 @@ public class ProjectThreeActivity extends Activity {
 
 		setContentView(R.layout.main);
 		buttonStateTextView = (TextView) findViewById(R.id.button_state_text_view);
-		
-		vibrator = ((Vibrator) getSystemService(VIBRATOR_SERVICE));
+
+        vibrator = ((Vibrator) getSystemService(VIBRATOR_SERVICE));
 	}
 
 	/**
@@ -116,31 +173,6 @@ public class ProjectThreeActivity extends Activity {
 		unregisterReceiver(mUsbReceiver);
 	}
 
-	private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
-			if (ACTION_USB_PERMISSION.equals(action)) {
-				synchronized (this) {
-					UsbAccessory accessory = (UsbAccessory) intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY); //mUsbManager.getAccessoryList()[0]; //UsbManager.getAccessory(intent);
-					if (intent.getBooleanExtra(
-							UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-						openAccessory(accessory);
-					} else {
-						Log.d(TAG, "permission denied for accessory "
-								+ accessory);
-					}
-					mPermissionRequestPending = false;
-				}
-			} else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
-				UsbAccessory accessory = (UsbAccessory) intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);//mUsbManager.getAccessoryList()[0]; //UsbManager.getAccessory(intent);
-				if (accessory != null && accessory.equals(mAccessory)) {
-					closeAccessory();
-				}
-			}
-		}
-	};
-
 	private void openAccessory(UsbAccessory accessory) {
 		mFileDescriptor = mUsbManager.openAccessory(accessory);
 		if (mFileDescriptor != null) {
@@ -167,57 +199,15 @@ public class ProjectThreeActivity extends Activity {
 			mAccessory = null;
 		}
 	}
-	
-	Runnable commRunnable = new Runnable() {
 
-		@Override
-		public void run() {
-			int ret = 0;
-			final byte[] buffer = new byte[3];
-
-			while (ret >= 0) {
-				try {
-					ret = mInputStream.read(buffer);
-				} catch (IOException e) {
-					break;
-				}
-
-				switch (buffer[0]) {
-				case COMMAND_BUTTON:
-
-					if(buffer[1] == TARGET_BUTTON) {
-						if(buffer[2] == VALUE_ON) {
-							startVibrate();
-						} else if(buffer[2] == VALUE_OFF){
-							stopVibrate();
-						}
-						runOnUiThread(new Runnable() {
-							
-							@Override
-							public void run() {
-								buttonStateTextView.setText(buffer[2] == VALUE_ON ? BUTTON_PRESSED_TEXT : BUTTON_NOT_PRESSED_TEXT);
-							}
-						});
-					}
-					break;
-
-				default:
-					Log.d(TAG, "unknown msg: " + buffer[0]);
-
-					break;
-				}
-			}
-		}
-	};
-	
 	public void startVibrate() {
 		if(vibrator != null && !isVibrating) {
 			isVibrating = true;
 			vibrator.vibrate(new long[]{0, 1000, 250}, 0);
 		}
 	}
-	
-	public void stopVibrate() {
+
+    public void stopVibrate() {
 		if(vibrator != null && isVibrating) {
 			isVibrating = false;
 			vibrator.cancel();
