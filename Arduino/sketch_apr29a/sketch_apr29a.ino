14082d195501285219d5602360a0de6703135d30
jordane-quincy <jordanequincy@gmail.com>
2016-04-30 11:07:34 +0200

    maj sketch

--- Arduino/sketch_apr29a/sketch_apr29a.ino
+++ Arduino/sketch_apr29a/sketch_apr29a.ino
@@ -2,11 +2,13 @@
 #include <Usb.h>
 #include <AndroidAccessory.h>
 
+#include <TinkerKit.h>
+
 #define COMMAND_BUTTON 0x1
 #define TARGET_BUTTON 0x1
 #define VALUE_ON 0x1
 #define VALUE_OFF 0x0
-#define INPUT_PIN 2
+#define INPUT_PIN I3
 
 AndroidAccessory acc("Manufacturer",
                      "Project03",
@@ -28,22 +30,37 @@ void setup() {
 
 void loop() {
   if (acc.isConnected()) {
-    Serial.print('A');
-    Serial.println();
     currentButtonState = digitalRead(INPUT_PIN);
-    if (currentButtonState == LOW) {
-      sntmsg[2] = VALUE_ON;
-    } else {
-      sntmsg[2] = VALUE_OFF;
-    }
-    Serial.print(sntmsg[0]);
-    Serial.println();
-    acc.write(sntmsg, 3);
-    lastButtonState = currentButtonState;
-    Serial.print('B');
-    Serial.println();
 
-    delay(100);
+    Serial.print(getVoltage(INPUT_PIN));
+    Serial.print("V :");
+    Serial.println(currentButtonState);
+
+    if (lastButtonState != currentButtonState) {
+      if (currentButtonState == LOW) {
+        sntmsg[2] = VALUE_ON;
+      } else {
+        sntmsg[2] = VALUE_OFF;
+      }
+
+      acc.write(sntmsg, 3);
+      lastButtonState = currentButtonState;
+    }
+
+    delay(1000);
+  } else {
+    Serial.print("AndroidAccessory not connected at ");
+    Serial.println(millis());
+    delay(2000);
   }
+}
+
+
+float getVoltage(int pin)
+{
+  return (analogRead(pin) * 0.004882814);
+  // This equation converts the 0 to 1023 value that analogRead()
+  // returns, into a 0.0 to 5.0 value that is the true voltage
+  // being read at that pin.
 }
 
