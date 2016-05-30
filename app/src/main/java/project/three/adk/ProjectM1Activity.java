57b539e07b8b7378eda5ce6620e645bb05dbb586
jordane-quincy <jordanequincy@gmail.com>
2016-05-30 17:30:24 +0200

    compte tour presque ok

--- app/src/main/java/project/three/adk/ProjectM1Activity.java
+++ app/src/main/java/project/three/adk/ProjectM1Activity.java
@@ -28,8 +28,8 @@ import it.moondroid.seekbarhint.library.SeekBarHint;
 
 public class ProjectM1Activity extends Activity {
 
-	private static final String TAG = ProjectM1Activity.class.getSimpleName();
-	private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private static final String TAG = ProjectM1Activity.class.getSimpleName();
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
     private PendingIntent mPermissionIntent;
     private boolean mPermissionRequestPending;
     private UsbManager mUsbManager;
@@ -41,13 +41,27 @@ public class ProjectM1Activity extends Activity {
     private SeekBarHint seekBar;
     private Button buttonSendDataView;
 
-	private Vibrator vibrator;
-	private boolean isVibrating;
+    private Vibrator vibrator;
+    private boolean isVibrating;
 
     private long prevMillis = 0;
     private final long intervalMillis = 2000;
     private long curMillis = 0;
     private float maxIr1VoltageSurUneSec = 0;
+
+    private float ancienVoltage = 0;
+    private float nouveauVoltage = 0;
+    private boolean isGrowing = true; // lorsque l'on stagne, on considère que c'est vrai aussi
+    private boolean isGrowingAncien = true;
+
+    private float ancienVoltageJ1 = 0;
+    private float nouveauVoltageJ1 = 0;
+    private final float SEUIL_VOLTAGE_J1 = 2.60f;
+
+    private float ancienVoltageJ2 = 0;
+    private float nouveauVoltageJ2 = 0;
+    private final float SEUIL_VOLTAGE_J2 = 1.70f;
+
     private int nbTourJoueur1 = 0;
     private int nbTourJoueur2 = 0;
     Runnable commRunnable = new Runnable() {
@@ -57,9 +71,9 @@ public class ProjectM1Activity extends Activity {
             int ret = 0;
 
             //buffer pour les donnees a recevoir de la carte arduino
-            final byte[] buffer = new byte[4+4];
+            final byte[] buffer = new byte[4 + 4];
 
-            while (ret >= 0) {
+            while (true) {
                 try {
                     ret = mInputStream.read(buffer);
                 } catch (IOException e) {
@@ -71,32 +85,47 @@ public class ProjectM1Activity extends Activity {
                 byteBuffer.position(0);
                 final float irVoltage = byteBuffer.getFloat();
 
-                if(maxIr1VoltageSurUneSec < irVoltage){
-                    maxIr1VoltageSurUneSec = irVoltage;
-                }
+                ancienVoltage = nouveauVoltage;
+                nouveauVoltage = irVoltage;
 
-                curMillis = System.currentTimeMillis();
-                //si deux secondes se sont écoulées
-                if ((curMillis - prevMillis) >= intervalMillis) {
-
-                    if(maxIr1VoltageSurUneSec >= 1.70){
+                isGrowingAncien = isGrowing;
+                isGrowing = nouveauVoltage > ancienVoltage;
+                if (!isGrowing & isGrowingAncien) {
+                    if (ancienVoltage > SEUIL_VOLTAGE_J1) {
                         nbTourJoueur1++;
+                    } else if (ancienVoltage > SEUIL_VOLTAGE_J2) {
+                        nbTourJoueur2++;
                     }
-
-                    //reset pour la prochaine seconde
-                    maxIr1VoltageSurUneSec = 0;
-                    prevMillis = System.currentTimeMillis();
                 }
 
+                /*
+                nouveauVoltageJ1 = irVoltage;
+                if(ancienVoltageJ1 < SEUIL_VOLTAGE_J1 && nouveauVoltageJ1 > SEUIL_VOLTAGE_J1){
+                    nbTourJoueur1++;
+                }
+
+                ancienVoltageJ2 = nouveauVoltageJ2;
+                nouveauVoltageJ2 = irVoltage;
+                if(ancienVoltageJ2 < SEUIL_VOLTAGE_J2 && nouveauVoltageJ2 > SEUIL_VOLTAGE_J2){
+                    nbTourJoueur2++;
+                }
+*/
                 //mise à jour de la vue utilisateur
                 runOnUiThread(new Runnable() {
 
                     @Override
                     public void run() {
                         String t = getApplicationContext().getResources().getString(R.string.nbTourJ1);
-                        buttonStateTextView.setText("msg length : "+ buffer.length +" = "+ irVoltage +"V "+"\r\n"+
-                                "tour j1 :"+ nbTourJoueur1 +"\r\n"+
-                                "tour j2 :"+ nbTourJoueur2);
+                        buttonStateTextView.setText("msg length : " + buffer.length + " = " + irVoltage + "V " + "\r\n" +
+                                        "tour j1 :" + nbTourJoueur1 + "\r\n" +
+                                        "tour j2 :" + nbTourJoueur2 + "\r\n" +
+                                        "isGrowingAncien : " + isGrowingAncien + "\r\n" +
+                                        "isGrowing : " + isGrowing + "\r\n" +
+                                        "ancienVoltage : " + ancienVoltage + "\r\n" +
+                                        "nouveauVoltage : " + nouveauVoltage + "\r\n" +
+                                        "SEUIL_VOLTAGE_J1 : " + SEUIL_VOLTAGE_J1 + "\r\n" +
+                                        "SEUIL_VOLTAGE_J2 : " + SEUIL_VOLTAGE_J2 + "\r\n"
+                        );
                     }
                 });
 
@@ -113,7 +142,7 @@ public class ProjectM1Activity extends Activity {
                     if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
                         openAccessory(accessory);
                     } else {
-                        Log.d(TAG, "permission denied for accessory "+ accessory);
+                        Log.d(TAG, "permission denied for accessory " + accessory);
                     }
                     mPermissionRequestPending = false;
                 }
@@ -126,17 +155,19 @@ public class ProjectM1Activity extends Activity {
         }
     };
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
         mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
-				ACTION_USB_PERMISSION), 0);
-		IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
-		filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
-		registerReceiver(mUsbReceiver, filter);
+                ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+        registerReceiver(mUsbReceiver, filter);
 
         setContentView(R.layout.main);
 
@@ -157,7 +188,7 @@ public class ProjectM1Activity extends Activity {
             public void onClick(View view) {
                 int seekBarValue = seekBar.getProgress();
 
-                Toast.makeText(getApplicationContext(), "sendData : "+ seekBarValue, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "sendData : " + seekBarValue, Toast.LENGTH_SHORT).show();
                 sendData(seekBarValue);
             }
         });
@@ -165,12 +196,12 @@ public class ProjectM1Activity extends Activity {
         buttonStateTextView = (TextView) findViewById(R.id.button_state_text_view);
 
         vibrator = ((Vibrator) getSystemService(VIBRATOR_SERVICE));
-	}
+    }
 
-    public void sendData(int nbBlink){
+    public void sendData(int nbBlink) {
         byte[] buffer = new byte[8];
 
-        buffer[0]= (byte) nbBlink; // button says off, light is on
+        buffer[0] = (byte) nbBlink; // button says off, light is on
 
         if (mOutputStream != null) {
             try {
@@ -179,87 +210,90 @@ public class ProjectM1Activity extends Activity {
             }
         }
     }
-	/**
-	 * Called when the activity is resumed from its paused state and immediately
-	 * after onCreate().
-	 */
-	@Override
-	public void onResume() {
-		super.onResume();
 
-		if (mInputStream != null && mOutputStream != null) {
-			return;
-		}
+    /**
+     * Called when the activity is resumed from its paused state and immediately
+     * after onCreate().
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
 
-		UsbAccessory[] accessories = mUsbManager.getAccessoryList();
-		UsbAccessory accessory = (accessories == null ? null : accessories[0]);
-		if (accessory != null) {
-			if (mUsbManager.hasPermission(accessory)) {
-				openAccessory(accessory);
-			} else {
-				synchronized (mUsbReceiver) {
-					if (!mPermissionRequestPending) {
-						mUsbManager.requestPermission(accessory,
-								mPermissionIntent);
-						mPermissionRequestPending = true;
-					}
-				}
-			}
-		} else {
-			Log.d(TAG, "mAccessory is null");
-		}
-	}
+        if (mInputStream != null && mOutputStream != null) {
+            return;
+        }
 
-	/** Called when the activity is paused by the system. */
-	@Override
-	public void onPause() {
-		super.onPause();
-		closeAccessory();
-	}
+        UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+        UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+        if (accessory != null) {
+            if (mUsbManager.hasPermission(accessory)) {
+                openAccessory(accessory);
+            } else {
+                synchronized (mUsbReceiver) {
+                    if (!mPermissionRequestPending) {
+                        mUsbManager.requestPermission(accessory,
+                                mPermissionIntent);
+                        mPermissionRequestPending = true;
+                    }
+                }
+            }
+        } else {
+            Log.d(TAG, "mAccessory is null");
+        }
+    }
 
-	/**
-	 * Called when the activity is no longer needed prior to being removed from
-	 * the activity stack.
-	 */
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		unregisterReceiver(mUsbReceiver);
-	}
+    /**
+     * Called when the activity is paused by the system.
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        closeAccessory();
+    }
 
-	private void openAccessory(UsbAccessory accessory) {
-		mFileDescriptor = mUsbManager.openAccessory(accessory);
-		if (mFileDescriptor != null) {
-			mAccessory = accessory;
-			FileDescriptor fd = mFileDescriptor.getFileDescriptor();
-			mInputStream = new FileInputStream(fd);
-			mOutputStream = new FileOutputStream(fd);
-			Thread thread = new Thread(null, commRunnable, TAG);
-			thread.start();
-			Log.d(TAG, "accessory opened");
-		} else {
-			Log.d(TAG, "accessory open fail");
-		}
-	}
+    /**
+     * Called when the activity is no longer needed prior to being removed from
+     * the activity stack.
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mUsbReceiver);
+    }
 
-	private void closeAccessory() {
+    private void openAccessory(UsbAccessory accessory) {
+        mFileDescriptor = mUsbManager.openAccessory(accessory);
+        if (mFileDescriptor != null) {
+            mAccessory = accessory;
+            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+            mInputStream = new FileInputStream(fd);
+            mOutputStream = new FileOutputStream(fd);
+            Thread thread = new Thread(null, commRunnable, TAG);
+            thread.start();
+            Log.d(TAG, "accessory opened");
+        } else {
+            Log.d(TAG, "accessory open fail");
+        }
+    }
+
+    private void closeAccessory() {
         manageArduinoDisconnected();
         try {
-			if (mFileDescriptor != null) {
-				mFileDescriptor.close();
-			}
-		} catch (IOException e) {
-		} finally {
-			mFileDescriptor = null;
-			mAccessory = null;
-		}
-	}
+            if (mFileDescriptor != null) {
+                mFileDescriptor.close();
+            }
+        } catch (IOException e) {
+        } finally {
+            mFileDescriptor = null;
+            mAccessory = null;
+        }
+    }
 
-	public void startVibrate() {
+    public void startVibrate() {
         vibrator.vibrate(1000); //vibre pendant 1000 ms
-	}
+    }
 
-    private void manageArduinoDisconnected(){
+    private void manageArduinoDisconnected() {
         Toast.makeText(getApplicationContext(), getApplicationContext().getString(R.string.arduinoDisconnected), Toast.LENGTH_LONG).show();
         startVibrate();
         //fermeture de cette activity
