70ac749fe1890b21011ce916dbfef4655efb45ca
jordane-quincy <jordanequincy@gmail.com>
2016-06-02 12:40:19 +0200

    fix typo / comment

--- Arduino/MCP41100/MCP41100.ino
+++ Arduino/MCP41100/MCP41100.ino
@@ -1,24 +1,22 @@
 /**********************************************************
-Sketch pour contrôler un potentiomètre numérique
-MCP41100 au moyen d'un Arduino Uno.
-http://electroniqueamateur.blogspot.ca/2013/04/faire-varier-une-resistance-au-moyen-de.html
+Sketch pour controler un potentiomètre numérique
+MCP41100 au moyen d'un Arduino Mega ADK.
 **********************************************************/
 
-
 #include <SPI.h>
- 
-const int slaveSelectPin = 53;
+
+#define SLAVE_SELECT_PIN 53 //sur l'arduino mega adk il est sur le pin 53
  
 void setup()
 {
   Serial.begin(115200);
-  pinMode (slaveSelectPin, OUTPUT);
+  pinMode (SLAVE_SELECT_PIN, OUTPUT);
   SPI.begin();
 }
  
 void loop()
 {
-/** declenche bien la voiture sur le circuit aussi (mais faut la lancer manuellement mais après c'est bon)*/
+  /** declenche bien la voiture sur le circuit aussi (mais faut la lancer manuellement mais après c'est bon)*/
    for (int i=252; i <= 255; i++){
     digitalPotWrite(i);
     delay(100);
@@ -28,8 +26,8 @@ void loop()
 int digitalPotWrite(int value)
 {
   Serial.print("digitalPotWrite ");Serial.print(value);Serial.print(" at ");Serial.println(millis());
-  analogWrite(slaveSelectPin, LOW);
-  SPI.transfer(0x11); 
+  digitalWrite(SLAVE_SELECT_PIN, LOW);
+  SPI.transfer(0x11);
   SPI.transfer(value);
-  digitalWrite(slaveSelectPin, HIGH);
+  digitalWrite(SLAVE_SELECT_PIN, HIGH);
 }
